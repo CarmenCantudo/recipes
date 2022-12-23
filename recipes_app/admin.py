0ab8ec660b5666877f222a3f53075bd7a22c4483
Carmen <carmencantudo@gmail.com>
2022-12-23 23:52:00 +0000

    Change summernote fields

--- recipes_app/admin.py
+++ recipes_app/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
+    """
+    Admin management from Admin Panel
+    Modified from I think therefore I blog, Code Institute
+    """
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'ingredients', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('description', 'ingredients', 'method')
 
-    summernote_fields = ('description', 'prep_time', 'cooking_time', 'serves',
-                         'ingredients', 'method')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Adds comment field in admin panel
+    Used from I think therefore I blog, Code Institute
+    """
+    list_display = ('name', 'body', 'recipe', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
