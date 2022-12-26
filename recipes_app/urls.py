3eb7a578a21c4e5ff0a763601639e57517c77e5a
Carmen <carmencantudo@gmail.com>
2022-12-26 19:02:09 +0000

    Add path to recipes list view

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
+    path('recipes/', views.RecipeList.as_view(), name='recipes'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
 ]
