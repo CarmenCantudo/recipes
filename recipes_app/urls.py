f8b4f18fa0d10778f6e38f259d716552f95a6370
Carmen <carmencantudo@gmail.com>
2022-12-28 19:12:30 +0000

    Connect recipe like path

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('recipes/', views.RecipeList.as_view(), name='recipes'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+    path('like/<slug:slug>/', views.RecipeLike.as_view(), name='recipe_like'),
 ]
