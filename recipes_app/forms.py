d46ef55caa74b1f4b32a485be90f4944acd6a497
Carmen <carmencantudo@gmail.com>
2023-01-01 22:50:31 +0000

    Add RecipeForm to add recipes

--- recipes_app/forms.py
+++ recipes_app/forms.py
@@ -1,4 +1,5 @@
-from .models import Comment
+from .models import Comment, Recipe
+from django_summernote.widgets import SummernoteWidget
 from django import forms
 
 
@@ -9,3 +10,29 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class RecipeForm(forms.ModelForm):
+    """
+    Add a Recipe Form
+    """
+    class Meta:
+        model = Recipe
+        fields = [
+            'title',
+            'description',
+            'serves',
+            'prep_time',
+            'cooking_time',
+            'ingredients',
+            'method',
+            'featured_image',
+        ]
+
+        widgets = {
+            'method': SummernoteWidget(),
+            'ingredients': SummernoteWidget(),
+        }
+
+        def __init__(self, *args, **kwargs):
+            super(RecipeForm, self).__init__(*args, **kwargs)
