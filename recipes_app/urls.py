81374ba86ceb77ff07c7bd452fd828591317b879
Carmen <carmencantudo@gmail.com>
2023-01-02 20:44:47 +0000

    Fix line too long

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.RecipeHome.as_view(), name='home'),
     path('recipes/', views.RecipeList.as_view(), name='recipes'),
     path('add_recipe/', views.AddRecipe.as_view(), name='add_recipe'),
-    path('edit_recipe/<int:pk>', views.EditRecipe.as_view(), name='edit_recipe'),
+    path('edit_recipe/<int:pk>', views.EditRecipe.as_view(), name='edit_recipe'
+         ),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('like/<slug:slug>/', views.RecipeLike.as_view(), name='recipe_like'),
 ]
